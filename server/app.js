8898a5d3dcb8a3c013792f1e7a4cd130f0c7419a
imclarisa <clarisaasmith@gmail.com>
2023-03-07 20:32:00 -0600

    PizzaSPA

--- server/app.js
+++ server/app.js
@@ -25,9 +25,24 @@ const logging = (request, response, next) => {
   next();
 };
 
-app.use(logging);
-app.use(express.json());
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
 
+app.use(cors);
+app.use(express.json());
+app.use(logging);
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
   // Create the headers for response by default 200
